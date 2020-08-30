4caed8a86d4c3bc9e76b28c301f3a3785668d1a8
Ronie Salgado <roniesalg@gmail.com>
2020-08-30 05:29:31 +0200

    Another typo.

--- scripts/packageArtifacts.sh
+++ scripts/packageArtifacts.sh
@@ -9,5 +9,5 @@ DIST=artifacts/dist/$PLATFORM_NAME
 
 mkdir -p "$DIST"
 cp -R pharo-vm pharo pharo-ui $DIST
-cp -R Pharo*.sources sysmel.* wbt wbt-ui $DIST
+cp -R Pharo*.sources wbt.* wbt wbt-ui $DIST
 cp -R LICENSE README.md $DIST
